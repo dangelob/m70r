2e596dc724860c575e4f958d384eca9e3cada177
Benoît D'Angelo <dev@dangelob.eu>
2014-08-31 14:32:56 +0200

    add diagplot function

---
+++ R/diagplot.R
@@ -0,0 +1,32 @@
+#' Set up diagnostic plots
+#' 
+#' This function print a diagnostic plot
+#'
+#' @param df data to proceed
+#' @param outname a name for the output pdf file, 
+#' by default the name is "diagPlot.pdf"
+#' @param outpath a path to a directory to save the file
+#' by default a "output" directory is created in the working directory to store
+#' the file
+#' 
+
+diagplot <- function(df, outname="diagPlot.pdf", outpath=NULL){
+  
+  if(is.null(outpath)){ #
+    dirls <- basename(list.dirs(getwd(), recursive=FALSE))
+    outpath <- paste0(getwd(), "/output/")
+    if ("output" %in% dirls){ # Is an output directory available
+    }else{
+      dir.create(outpath)
+    }
+    savpath <- paste0(outpath, outname)
+  }else{
+    savpath <- paste0(chk_eop(outpath), outname)
+  }
+  
+  pdf(savpath)
+  for(i in unique(df$fileid)){
+    diagtmplt(df[which(df$fileid == i),], i)
+  }  
+  graphics.off()
+}
