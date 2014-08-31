43ff9f53c720c45acbd40fa7371d98a9bc6780f5
Benoît D'Angelo <dev@dangelob.eu>
2014-08-31 14:12:24 +0200

    add readm70 function

---
+++ R/readm70.R
@@ -0,0 +1,13 @@
+#' Read ".m70" files
+#' 
+#' This function read the .m70 files
+#' 
+#'
+#' @param path path to the file to clean
+#' 
+
+read.m70 <- function(path){
+  df <- read.table(path, sep="", skip=9) # Read file without the 1st 9 lines
+  df <- colchk(df) # Check column order
+  return(df) 
+}
