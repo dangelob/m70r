0231fad5ef3006fc0550a867eab2b8c7b0fd0e80
Benoît D'Angelo <dev@dangelob.eu>
2014-08-31 13:21:42 +0200

    add m70setup function

---
+++ R/m70setup.R
@@ -0,0 +1,21 @@
+#' Data preparation
+#' 
+#' Input a path to a directory with m70 files. The function will prepare the data
+#' for visualisation. By defaut m70 files will be converted in csv and written in 
+#' an CO2_csv directory, a csv file with all the data will be written in an "output"
+#' directory 
+#'
+#' @param path Path to the directory to treat
+#' 
+
+## TODO
+# Retrait de la copie en csv (m70tocsv) − DONE
+# Mettre la fonction a disposition quand même dans le package
+
+m70setup <- function(path){
+  
+  m70filename(path)
+  df <- ccat(path, id="name")
+  return(df)
+  
+}
